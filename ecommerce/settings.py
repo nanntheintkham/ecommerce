4ffc8b1d9c77e03a9a9979b168d38b85e22a9bd4
Nann <nannkham112@gmail.com>
2025-01-20 11:36:32 +0100

    bug fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -208,6 +208,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_STORAGE_BUCKET_NAME = 'euphoria-media'
 
 AWS_S3_CUSTOM_DOMAIN = 'euphoria-media.s3.amazonaws.com'
 if DEBUG:

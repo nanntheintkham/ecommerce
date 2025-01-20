3a80db6a8c04da1e443a1d63aa2f867a72b9c570
Nann <nannkham112@gmail.com>
2025-01-20 11:24:15 +0100

    bug fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -216,7 +216,7 @@ if DEBUG:
 
 else:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-   # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
 
 # Default primary key field type
--- store/models.py
+++ store/models.py
@@ -62,7 +62,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='upload/products', null=True, blank=True, storage=S3Boto3Storage())
+    image = models.ImageField(upload_to='upload/products', null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     product_type = models.CharField(max_length=10, choices=PRODUCT_TYPE_CHOICES, default='physical')
     stock = models.IntegerField(default=0)

1dfb98c2f335d065b56ae4791254236e35f3b37d
Nann <nannkham112@gmail.com>
2025-01-20 11:51:15 +0100

    bug fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -203,21 +203,25 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files storage
-# S3 Static and Media Settings
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_ADDRESSING_STYLE = 'virtual'
-AWS_STORAGE_BUCKET_NAME = 'euphoria-media'
+USE_S3 = os.environ.get('USE_S3') == 'TRUE'
 
-AWS_S3_CUSTOM_DOMAIN = 'euphoria-media.s3.amazonaws.com'
+if USE_S3:
+    # S3 Static and Media Settings
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_ADDRESSING_STYLE = 'virtual'
+    AWS_STORAGE_BUCKET_NAME = 'euphoria-media'
+    AWS_S3_CUSTOM_DOMAIN = 'euphoria-media.s3.amazonaws.com'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 if DEBUG:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 else:
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
     
 
 # Default primary key field type
